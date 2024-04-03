7d88bca23a861ff4acf2f2dc6b34e6ba89378e00
ArDa0226 <161866557+ArDa0226@users.noreply.github.com>
2024-04-03 19:52:47 +0500

    ДЗ по теме: "Наследование классов"
    Изменено переопределение метода "Лошадиная сила"

--- DZ_nasledovanie.py
+++ DZ_nasledovanie.py
@@ -1,26 +1,35 @@
 
 class Car:
     price = 1000000
-    def horse_powers(self, power):
-        self.pw = power
+    def horse_powers(self):
+        
+        print('У машины 200 л.с.')
+
 
 
 
 class Nissan(Car):
 
     def __str__(self):
-        return f'{self.__class__.__name__}: price = {self.price}, horse power = {self.pw}'
+        return f'{self.__class__.__name__}: price = {self.price}'
+
+    def horse_powers(self):
+        print(f'У машины {self.__class__.__name__} 160 л.с.')
+
 class Kia(Car):
 
     def __str__(self):
-        return f'{self.__class__.__name__}: price = {self.price}, horse power = {self.pw}'
+        return f'{self.__class__.__name__}: price = {self.price}'
+
+    def horse_powers(self):
+        print(f'У машины {self.__class__.__name__} 140 л.с.')
 
 nissan = Nissan()
 nissan.price = 1500000
-nissan.horse_powers(186)
 print(nissan)
+nissan.horse_powers()
 
 kia= Kia()
 kia.price = 1300000
-kia.horse_powers(155)
 print(kia)
+kia.horse_powers()
